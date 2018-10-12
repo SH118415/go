424710a30306bbde1f3f366b809de4cb98be743f
Asma <asma@localhost.localdomain>
2018-10-12 23:24:18 +0530

    Hello World

---
+++ hello_world/return_value/1/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main () {
+	n,err:=fmt.Println("Hello World")
+// Prints the number of bytes
+	fmt.Println(n) 
+//Checks if there is any error
+	fmt.Println(err)
+}
---
+++ hello_world/return_value/2/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main () {
+// underscore character tells the compiler that we need catch something but ignore it
+	n,_ := fmt.Println("Hello World")
+	fmt.Println(n)
+}
