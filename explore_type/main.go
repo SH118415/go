cfdbf2a2d067118b3dd144df211866c3ca300deb
Asma <asma@localhost.localdomain>
2018-10-12 23:31:50 +0530

    type

---
+++ explore_type/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var x = 20
+	var y = "Hello"
+	fmt.Printf("%T\n", x)
+	fmt.Printf("%T\n", y)
+	// variable can be declared of certain Type
+	x =30
+
+}
+
