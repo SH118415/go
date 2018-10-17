afc217e31c7132633fe61e2c14b42aec2ce55204
Asma <asma@localhost.localdomain>
2018-10-18 01:03:01 +0530

    example for constants

---
+++ constant/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// constants are variables whose values cannot change later
+	const a = 10
+	const b = 42.32
+	const c = "Hello"
+	fmt.Println(a,b,c)
+}
