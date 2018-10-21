a6c802b5f1b5955aa8f4e6118987e937551cf044
Asma <asma@localhost.localdomain>
2018-10-21 13:32:35 +0530

    slice-for range

--- types/slice_type/for_range/main.go
+++ types/slice_type/for_range/main.go
@@ -4,9 +4,13 @@ import "fmt"
 
 func main() {
 	x := []int{1, 2, 3, 4, 5}
+
 	// i is the index and v is the value
 	for i, v := range x {
 		fmt.Println(i,v)
 	}
+	/* for i := 0; i <= 4; i++ {
+		fmt.Println(i, x[i])
+	} */
 }
 
