85b6e96774856af693417102062b4b820db596c8
Asma <asma@localhost.localdomain>
2018-10-29 01:01:02 +0530

    func attached to a type

---
+++ types/methods/1/main.go
@@ -0,0 +1,48 @@
+// In between the keyword func and the name of the function we've added a “receiver”.
+// The receiver is like a parameter – it has a name and a type 
+// creating the function in this way allows us to call the function using the . operator
+
+
+package main
+
+import (
+	"fmt"
+)
+
+type person struct {
+	first string
+	last  string
+}
+
+type secretAgent struct {
+	person
+	ltk bool
+}
+
+// func (r receiver) identifier(parameters) (return(s)) { code }
+
+func (s secretAgent) speak() {
+	fmt.Println("I am", s.first, s.last)
+}
+
+func main() {
+	sa1 := secretAgent{
+		person: person{
+			"James",
+			"Bond",
+		},
+		ltk: true,
+	}
+
+	sa2 := secretAgent{
+		person: person{
+			"Miss",
+			"Moneypenny",
+		},
+		ltk: true,
+	}
+
+	fmt.Println(sa1)
+	sa1.speak()
+	sa2.speak()
+}
