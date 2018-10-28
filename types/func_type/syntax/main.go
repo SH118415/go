3af2dc9a14aa66747fcf15a7deb3e180b31683dd
Asma <asma@localhost.localdomain>
2018-10-28 11:00:14 +0530

    function

---
+++ types/func_type/syntax/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	foo()
+	bar("James")
+	s1 := woo("Moneypenny")
+	fmt.Println(s1)
+}
+
+func foo() {
+	fmt.Println("hello from foo")
+}
+
+func bar(s string) {
+	fmt.Println("Hello,", s)
+}
+
+func woo(st string) string {
+	return fmt.Sprint("Hello from woo, ", st)
+}
+
